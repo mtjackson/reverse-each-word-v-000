ee4a1429f8400052f9ff3111be7a0a0fa6904754
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-26 15:50:34 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,10 @@
 def reverse_each_word(sentence)
   split_sentence = []
+  reversed_sentence = []
   split_sentence = sentence.split
   split_sentence.collect do | word |
     split_words = word.split("")
     split_words.reverse.join
   end
-  split_words.join(" ")
+  puts
 end
